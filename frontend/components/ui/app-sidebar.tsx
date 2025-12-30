b2788bf9211ef92ad48764bf7b8e128f628b9f38
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-30 14:02:56 +0530

    add shadcn setup and components

---
+++ frontend/components/ui/app-sidebar.tsx
@@ -0,0 +1,78 @@
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { Gavel, CircleDollarSign, Settings, LayoutDashboard, User2, ChevronUp } from "lucide-react"
+
+const items = [
+    {
+        title: 'Dashboard',
+        url: '/',
+        icon: LayoutDashboard
+    },
+
+    {
+        title: 'Auctions',
+        url: '/auctions',
+        icon: Gavel,
+    },
+    {
+        title: 'Bidding',
+        url: '/bid',
+        icon: CircleDollarSign,
+    },
+    {
+        title: 'Settings',
+        url: '/settings',
+        icon: Settings
+    }
+]
+
+export function AppSidebar() {
+    return (
+        <Sidebar>
+            <SidebarGroupLabel> Auction Service </SidebarGroupLabel>
+
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarMenu>
+                        {items.map((item) => (
+                            <SidebarMenuItem key={item.title}>
+                                <SidebarMenuButton asChild>
+                                    <a href={item.url}>
+                                        <item.icon />
+                                        <span>{item.title}</span>
+                                    </a>
+                                </SidebarMenuButton>
+                            </SidebarMenuItem>
+                        ))}
+                    </SidebarMenu>
+                </SidebarGroup>
+            </SidebarContent>
+
+            <SidebarFooter>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <SidebarMenuButton suppressHydrationWarning={true}>
+                                    <User2 /> Username
+                                    <ChevronUp className="ml-auto" />
+                                </SidebarMenuButton>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent side="top" className="w-[--radix-popper-anchor-width]">
+
+                                <DropdownMenuItem>
+                                    <span>Account Details</span>
+                                </DropdownMenuItem>
+
+                                <DropdownMenuItem>
+                                    <span>Sign out</span>
+                                </DropdownMenuItem>
+
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarFooter>
+        </Sidebar>
+    )
+}
