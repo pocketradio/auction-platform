8276a4bd5ffbe4b071d3681fb90c071fe8c3a6b3
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2026-01-17 15:00:53 +0530

    feat : overhaul sse logic to multiplexed room based streams with heartbeats

--- backend/routes/stream.router.ts
+++ backend/routes/stream.router.ts
@@ -1,25 +1,63 @@
-// import type { Auction } from "@prisma/client";
 import { Router } from "express";
 const streamRouter = Router();
 import type { Response } from "express";
 
-const clients : Response[] = [];
+const auctionRoom = new Map<string, Response[]>();
 
-streamRouter.get('/', (req,res) => {   
+streamRouter.get('/lobby', (req, res) => {
     res.setHeader("Content-Type", "text/event-stream");
     res.setHeader("Cache-Control", "no-cache");
     res.setHeader("Connection", "keep-alive");
     res.flushHeaders();
-    clients.push(res);
-    console.log(clients,"is the list of clients");
-    req.on("close", ()=>{
-        const index = clients.indexOf(res);
-        if ( index > -1){
-            clients.splice(index, 1);
+
+    auctionRoom.get('lobby')?.push(res) ?? auctionRoom.set('lobby', [res]);
+
+    req.on("close", () => {
+        const updatedLobby = auctionRoom.get('lobby')?.filter((element) => element !== res);
+        if (updatedLobby !== undefined && updatedLobby.length > 0) {
+            auctionRoom.set('lobby', updatedLobby);
+        }
+        else {
+            auctionRoom.delete('lobby');
+        }
+    })
+})
+
+streamRouter.get('/:id', (req, res) => {
+
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+    res.flushHeaders();
+
+    const auctionId = String(req.params.id);
+    auctionRoom.get(auctionId)?.push(res) ?? auctionRoom.set(auctionId, [res]);
+
+
+    req.on("close", () => {
+        const updatedRoom = auctionRoom.get(auctionId)?.filter((element) => element !== res);
+        if (updatedRoom !== undefined && updatedRoom.length > 0) {
+            auctionRoom.set(auctionId, updatedRoom);
+        }
+        else {
+            auctionRoom.delete(auctionId);
         }
     })
 })
 
 
+//heartbeat keep alive
+setInterval(() => {
 
-export {streamRouter, clients};
+    auctionRoom.forEach((value, key) => {
+        value.forEach(element => {
+            element.write(': heartbeat\n\n')
+        });
+    });
+
+    console.log("Heartbeat sent to all active rooms.")
+}, 25000);
+
+
+
+export { streamRouter, auctionRoom };
